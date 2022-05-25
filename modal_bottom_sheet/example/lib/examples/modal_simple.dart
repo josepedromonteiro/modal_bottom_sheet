6060fd13bb72a8d6d39e6a6c2bbd6374775252de
Pedro Monteiro <josepedromonteiro@outlook.com>
2022-05-25 15:23:53 +0100

    First version

---
+++ modal_bottom_sheet/example/lib/examples/modal_simple.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SimpleModal extends StatelessWidget {
+  final ScrollController scrollController;
+
+  const SimpleModal({Key? key, required this.scrollController})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+          leading: Container(), middle: Text('Modal Page')),
+      child: Center(),
+    ));
+  }
+}
